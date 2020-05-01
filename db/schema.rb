e90914d5be3dcdc8b369d3f05cafb99f2ec0cfa8
kamioka24 <keee2404@gmail.com>
2020-05-01 18:38:56 +0900

    ヘッダーリンク作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_060503) do
+ActiveRecord::Schema.define(version: 2020_04_30_102233) do
+
+  create_table "post_books", force: :cascade do |t|
+    t.string "title"
+    t.text "opinion"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
